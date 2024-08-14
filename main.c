b7e9593e467c7eae727c14c260a659d7eabbf0e8
Mariosito2000 <marioariasespinosa@gmail.com>
2024-08-14 16:58:24 +0200

    main test added

--- main.c
+++ main.c
@@ -3,6 +3,17 @@
 int main ()
 {
     printf("%ld\n", ft_strlen("hola"));
+
+
+    printf("Text to split:  Hello world. Have a lovely day.\n\n");
     
+    char **splitted_text = ft_split("Hello world. Have a lovely day.", ' ');
+    
+    printf("Splitted text:\n\n");
+    for (int i = 0; splitted_text[i]; i++)
+        printf("%s\n", splitted_text[i]);
+
+    printf("\nSplitted text end\n");
+
     return (0);
 }
